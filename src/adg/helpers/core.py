e51d42086e2800cdf70a1f89738e412239185ee5
Den Delimarsky <1389609+dend@users.noreply.github.com>
2019-11-08 14:33:32 -0800

    Update core.py

--- src/adg/helpers/core.py
+++ src/adg/helpers/core.py
@@ -125,7 +125,7 @@ class LibraryProcessor():
                 "cd " + target_library_directory + f" && ./../../../../../{os.path.join('bin', 'sphinx-quickstart')} -q -p 'adg' -a 'automated' -v '1.0'", shell=True)))
         elif operating_system == OperatingSystem.windows:
             print(Util.pretty_stdout(subprocess.check_output(
-                "powershell.exe cd " + target_library_directory + f"; & ..\\..\\..\\{os.path.join('Scripts', 'sphinx-quickstart.exe')} -q -p 'adg' -a 'automated' -v '1.0'", shell=True)))
+                "powershell.exe cd " + target_library_directory + f"; & ..\..\..\..\{os.path.join('Scripts', 'sphinx-quickstart.exe')} -q -p 'adg' -a 'automated' -v '1.0'", shell=True)))
 
         # We need to update the configuration file for Sphinx,
         # to make sure that we're documenting the right library.
