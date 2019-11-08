3110e97b9f734775f4264c91fae87d194e13a407
Den Delimarsky <1389609+dend@users.noreply.github.com>
2019-11-08 14:26:53 -0800

    Update core.py

--- src/adg/helpers/core.py
+++ src/adg/helpers/core.py
@@ -125,7 +125,7 @@ class LibraryProcessor():
                 "cd " + target_library_directory + f" && ./../../../../../{os.path.join('bin', 'sphinx-quickstart')} -q -p 'adg' -a 'automated' -v '1.0'", shell=True)))
         elif operating_system == OperatingSystem.windows:
             print(Util.pretty_stdout(subprocess.check_output(
-                "powershell.exe Get-Location ; cd " + target_library_directory, shell=True)))
+                "powershell.exe Get-Location ; cd " + target_library_directory + "; Get-Location", shell=True)))
 
 # + f" ; & ..\\..\\..\\..\\{os.path.join('Scripts', 'sphinx-quickstart.exe')} -q -p 'adg' -a 'automated' -v '1.0'
 
